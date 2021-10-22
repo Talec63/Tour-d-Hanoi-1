3ed534d1833cc8ef639b6fc563c8f8eabe803fe8
Talec <62483833+Talec63@users.noreply.github.com>
2021-10-22 06:50:06 +0000

    Hanoi function

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-print("test")
+def Hanoi(n, dep, inter, arr):
+  if n == 0:
+    return 0
+  else:
+    Hanoi(n-1, dep, inter, arr)
+    print("Déplacer le disque {} de la tige {} vers la tige {}.".format(n,dep,arr))
+    Hanoi(n − 1, dep, inter, arr)
 
-def test():
-  print("test2")
-
+n = 10
+dep = "a"
+inter = "b"
+arr = "c"
+print(Hanoi(n, dep, inter, arr))
